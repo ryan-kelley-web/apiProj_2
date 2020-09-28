27797eb0f5f1fd14f1131eca71251a81ca7054b1
Ryan Kelley <ryan.kelley.1223@gmail.com>
2020-09-28 17:14:58 -0400

    api2 commit1

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ const key = 'r-XlgVpb8Q9VzsEGsbSNhJKvF8eIyCipfqFBuav-OHk';
 const secret_key = 'I4epuxODd0hqCtNLYedSqqg4e3D3C1o0mRIMMlC8f50';
 
 
-fetch('https://api.unsplash.com/photos/?client_id=r-XlgVpb8Q9VzsEGsbSNhJKvF8eIyCipfqFBuav-OHk')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function seeMainData() {
-        return console.log(json);
-    })
-
-    seeMainData();
+fetch(baseURL)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
